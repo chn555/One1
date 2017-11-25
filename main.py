7cd149818e1666798585b12bc6705924b8f98e4c
chn555 <chn566@gmail.com>
2017-11-25 17:48:49 +0200

    added comments, req and defined as function

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ import urllib3
 import re
 urllib3.disable_warnings()
 
+def get_call_number():
 
-r = requests.get("https://62.0.34.68/benefitmobile/cust/one1/frmwo.aspx?tel=0524098844", verify=False)
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
+# get the page and parse it
+    r = requests.get("https://62.0.34.68/benefitmobile/cust/one1/frmwo.aspx?tel=0524098844", verify=False)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
 
-number = soup.findAll("span", {"id" : "lblTitle"}, text=True)[0].text
-number = re.sub("\D", "", number)
+# call number as global, to be used outside this function
+    global number
 
+# remove all the text, leave only the numbers
+    number = soup.findAll("span", {"id" : "lblTitle"}, text=True)[0].text
+    number = re.sub("\D", "", number)
 
+get_call_number()
 print number
 
