933a7a26c902d3c5540b4cea1fc589b7464e4df3
chn555 <chn566@gmail.com>
2018-02-20 19:51:45 +0200

    yes

--- main.py
+++ main.py
@@ -1,63 +1,36 @@
+#!/usr/bin/env python2
+import sys
+reload (sys)
+sys.setdefaultencoding('utf8')
 from bs4 import BeautifulSoup
 import requests
 import urllib3
 import re
 import time
+import datetime
 from requests.exceptions import ConnectionError
 urllib3.disable_warnings()
+import nexmo
+client = nexmo.Client(key="2c70516f", secret="0a7a75d30074b55f")
 
-def get_phone_number():
-    global phone_number
-    phone_number = raw_input("Enter phone number : ")
+file = open("/home/chn555/One1/pnum.txt","r")
+text = open("/home/chn555/One1/callnum.txt","r")
 
-    if phone_number == "1" :
-        phone_number = "0524098844"
-    elif len(phone_number) != 10:
-        print ("Invalid phone number, try again.")
-        get_phone_number()
-    else:
-        pass
+phone_number = int(file.read())
+old_call_number = int(text.read())
+global global_phone_number
+global_phone_number = "972"+str(phone_number)
+print global_phone_number
 
-    return str(phone_number)
 
-def get_email():
-    global Email
-    Email = raw_input("Enter e-mail address : ")
-    return Email
-
-def get_call_number():
-    global phone_number
-    try :
-        #get the page and parse it
-        r = requests.get("https://62.0.34.68/benefitmobile/cust/one1/frmwo.aspx?tel={}".format(phone_number), verify=False)
-        data = r.text
-        soup = BeautifulSoup(data, "html.parser")
-# call number as global, to be used outside this function
-        global number
-
-# remove all the text, leave only the numbers
-        number = soup.findAll("span", {"id" : "lblTitle"}, text=True)[0].text
-        number = re.sub("\D", "", number)
-
-    except IndexError:
-        print ("Something went wrong, probably wrong phone number.")
-    except ConnectionError:
-        print ("System is offline, retrying in 5min")
-        time.sleep(300)
-        get_call_number()
-
-    return number
-# copied from stackoverflow
 def send_email(user, pwd, recipient, subject, body):
     import smtplib
-
     gmail_user = user
     gmail_pwd = pwd
     FROM = user
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
-
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
@@ -72,43 +45,98 @@ def send_email(user, pwd, recipient, subject, body):
     except:
         print ("failed to send mail")
 
-msg = "nothing"
-def call_checker():
-    global Email
-    send_email(user="chn566", pwd="itwmedbwphqaklsc", recipient=Email, subject="One1 Call Notification System", body="Notification system started")
-    while True :
-        #calls number
+def get_call_number():
+    global phone_number
+
+    try :
+        # get the page and parse it
+        r = requests.get("https://62.0.34.68/benefitmobile/cust/one1/frmwo.aspx?tel=0{}".format(phone_number),
+                         verify=False)
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+        # call number as global, to be used outside this function
+
+        number = 0
         global number
-        print (number)
-        updated_calls = get_call_number()
-        print (updated_calls)
-        global msg
-        if updated_calls == number:
-            pass
-        elif updated_calls > number:
-            diff = int(updated_calls) - int (number)
-            msg = "{} call(s) added, {} calls total.".format(diff, updated_calls)
+        # remove all the text, leave only the numbers
+        number = soup.findAll("span", {"id" : "lblTitle"}, text=True)[0].text
+        print number
+        number = re.sub("\D", "", number)
+        print number
 
-            send_email(user="chn566", pwd="itwmedbwphqaklsc", recipient=Email,
-                       subject="One1 Call Notification System", body=msg)
-            number = updated_calls
-        elif number > updated_calls:
-            diff = int(number) - int(updated_calls)
-            msg = "{} call(s) removed, {} calls total.".format(diff, updated_calls)
-
-            send_email(user="chn566", pwd="itwmedbwphqaklsc", recipient=Email,
-                       subject="One1 Call Notification System", body= msg)
-            number = updated_calls
-        print (msg)
-        time.sleep(60)
+    except IndexError:
+        print ("Something went wrong, probably wrong phone number.")
+    except ConnectionError:
+        print ("System is offline, retrying in 5min")
+        time.sleep(300)
+        get_call_number()
 
 
-phone_number = get_phone_number()
-Email = str(get_email())
-get_call_number()
-number = int(get_call_number())
-call_checker()
+    return number
 
-#send_email(user = "chn566",pwd= "itwmedbwphqaklsc",recipient= "chn566work@gmail.com",subject= "One1 Call Notification System",body= "Current number of calls is {}".format(number))
-print (number)
+def compare_calls () :
 
+    global msg
+    msg = "test msg"
+    if current_call_number == old_call_number:
+       pass
+    elif current_call_number > old_call_number:
+        diff = int(current_call_number) - int(old_call_number)
+        msg = "{} call(s) added, {} calls total.\n"\
+              "{:%H:%M}".format(diff, current_call_number, datetime.datetime.now())
+        send_SMS(global_phone_number, msg)
+        print msg
+
+    elif old_call_number > current_call_number:
+        diff = int(old_call_number) - int(current_call_number)
+        msg = "{} call(s) removed, {} calls total.\n" \
+              "{:%H:%M}".format(diff, current_call_number, datetime.datetime.now())
+        send_SMS(global_phone_number, msg)
+        print msg
+
+def compare_calls_email () :
+
+    global msg
+    msg = "test msg"
+    if current_call_number == old_call_number:
+       pass
+    elif current_call_number > old_call_number:
+        diff = int(current_call_number) - int(old_call_number)
+        msg = "{} call(s) added, {} calls total.\n"\
+              "{:%H:%M}".format(diff, current_call_number, datetime.datetime.now())
+        send_email(user="chn566", pwd="itwmedbwphqaklsc", recipient="chn566work@gmail.com",
+               subject="One1 Call Notification System", body=msg)
+        print msg
+
+    elif old_call_number > current_call_number:
+        diff = int(old_call_number) - int(current_call_number)
+        msg = "{} call(s) removed, {} calls total.\n" \
+              "{:%H:%M}".format(diff, current_call_number, datetime.datetime.now())
+        send_email(user="chn566", pwd="itwmedbwphqaklsc", recipient="chn566work@gmail.com",
+                   subject="One1 Call Notification System", body=msg)
+        print msg
+
+def send_SMS(x,y):
+    client.send_message({
+        'from': 'One1',
+        'to': x,
+        'text': y,
+    })
+
+current_call_number = int(get_call_number())
+
+
+compare_calls()
+
+text.close()
+
+
+
+
+
+
+
+text = open("/home/chn555/One1/callnum.txt","w")
+text.write(str(current_call_number))
+file.close()
+text.close()
